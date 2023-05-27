3579509d58a88889354c557b67ad26e26a351f43
inwiziym <zadorozhnydenis@mail.ru>
2023-05-27 15:43:21 +0700

    0.0.5
    New anim attack

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
-    public float crouchSpeed; // скорость передвижения в приседе
-    public float crouchScale; // масштаб при приседании
+    public float crouchSpeed;
+    public float crouchScale;
     private Rigidbody2D rb;
     private Animator anim;
-    private bool isCrouching = false; // флаг состояния приседания
-    private bool isJumping = false; // флаг состояния прыжка
-
+    private bool isCrouching = false;
+    private bool isJumping = false;
+    private float attackTime = 0f;
     public GameObject goWin;
 
     void Start()
@@ -52,26 +52,40 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping) // Добавлено условие для проверки состояния прыжка
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             anim.SetBool("Jump", true);
-            isJumping = true; // Установка флага прыжка в true
+            isJumping = true;
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
             isCrouching = true;
             anim.SetBool("IsCrouching", true);
-            rb.velocity = Vector2.zero; // остановка персонажа при приседании
-            transform.localScale = new Vector3(1f, crouchScale, 1f); // изменение масштаба при приседании
+            rb.velocity = Vector2.zero;
+            transform.localScale = new Vector3(1f, crouchScale, 1f);
         }
         else if (Input.GetKeyUp(KeyCode.S))
         {
             isCrouching = false;
             anim.SetBool("IsCrouching", false);
-            transform.localScale = new Vector3(1f, 1f, 1f); // возвращаем обычный масштаб при вставании
+            transform.localScale = new Vector3(1f, 1f, 1f);
         }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            attackTime = 0f; // Сбросить время, прошедшее с начала атаки
+            anim.SetBool("IsPunch", true);
+        }
+
+        // Если анимация атаки активна и прошла 1 секунда, остановить анимацию
+        if (anim.GetBool("IsPunch") && attackTime >= 0.5f)
+        {
+            anim.SetBool("IsPunch", false);
+        }
+
+        attackTime += Time.deltaTime; // Увеличить время, прошедшее после нажатия клавиши J
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -79,7 +93,7 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             anim.SetBool("Jump", false);
-            isJumping = false; // Сброс флага прыжка при касании земли
+            isJumping = false;
         }
     }
 }
